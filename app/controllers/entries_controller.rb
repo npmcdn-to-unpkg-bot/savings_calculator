c44d3e132e0a6f63fe17ed2fa636adeb2ccb8dd0
Pal Makkar <Pal@Pals-MacBook-Pro.local>
2016-06-30 12:17:52 -0700

    added two conditions for provider and calculated the bill

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,9 +7,17 @@ class EntriesController < ApplicationController
   	@entry = Entry.create(entry_params)
   	@zip = @entry.zip
   	@provider = @entry.provider
+  	@usage = @entry.monthly_energy_usage
+
   	@providers = RateProvider.where(zip: @zip)
   	@subregion = ZipSubregion.find_by_zip(@zip)
-  	# @rate = RateProvider.find_by_provider(@provider)
+  	#@rate = RateProvider.where(zip: @zip).first
+  	@rate = RateProvider.where('zip = ? AND provider = ?', params[:entry][:zip], params[:entry][:provider]).first
+  	 @bill = @usage * @rate.rate
+  	# } else {
+  	# 	redirect_to root_path
+  	# }
+
 
   	if @entry.persisted?
 			flash.now[:success] = "Your entry was recorded!"
