51e9009e1c833979f9b2974310d982d9e835f266
Pal Makkar <Pal@Pals-MacBook-Pro.local>
2016-06-30 16:01:31 -0700

    Styled entry controller code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -13,27 +13,46 @@ class EntriesController < ApplicationController
 
   	@providers = RateProvider.where(zip: @zip)
   	@subregion = ZipSubregion.find_by_zip(@zip)
-  	@heating_rate = FuelCost.find_by_fuel_type(@fuel_type).cost
+
+  	#Fuel information
+  	@fuel = FuelCost.find_by_fuel_type(@fuel_type)
+  	@heating_rate = @fuel.cost
+  	@raw_heating_emissions = @fuel.emissions
+  	@emission_conversion = @fuel.conversion
   	@sr_emissions = SubregionEmission.find_by_acronym(@subregion.primary_sub)
 
+  	#If provider serves zip continue, else print error
   	if (@rate = RateProvider.where('zip = ? AND provider = ?', params[:entry][:zip], params[:entry][:provider]).first)
+
+  		#Calculating total bill
   		@electric_bill = @electric_usage * @rate.rate
   		@heating_bill = @heating_usage * @heating_rate
   		@total_bill = @electric_bill + @heating_bill
 
-
-
+  		#Calculating reduced electric bill
   		@electric_reduced = @electric_usage * (1 - (0.164 * 0.8))
-  		@heating_reduced = @heating_usage * 0.88
   		@reduced_electric_bill = @electric_reduced * (@rate.rate + 0.015)
+
+  		#Calculating reduced heating bill
+  		@heating_reduced = @heating_usage * 0.88
   		@reduced_heating_bill = @heating_reduced * @heating_rate
+
+  		#Total savings
   		@total_reduced_bill = @reduced_electric_bill + @reduced_heating_bill
   		@total_savings = @total_bill - @total_reduced_bill
-  		@heat_emissions = FuelCost.find_by_fuel_type(@fuel_type).emissions * @heating_usage * FuelCost.find_by_fuel_type(@fuel_type).conversion
-  		@reduced_heat_emissions = FuelCost.find_by_fuel_type(@fuel_type).emissions * @heating_reduced * FuelCost.find_by_fuel_type(@fuel_type).conversion
-  		@total_emissions = ((@electric_usage * @sr_emissions.emissions) / 1000) + @heat_emissions
-  		@reduced_emissions = ((@electric_reduced * @sr_emissions.emissions) / 1000) + @reduced_heat_emissions
+
+
+  		#Calculating reduced emissions
+  		@electric_emissions = (@electric_usage * @sr_emissions.emissions) / 1000
+  		@reduced_electric_emissions = ((@electric_reduced * @sr_emissions.emissions) / 1000)
+  		@heat_emissions = @raw_heating_emissions * @heating_usage * @emission_conversion
+  		@reduced_heat_emissions =  @raw_heating_emissions * @heating_reduced * @emission_conversion
+
+  		#Total reductions in emissions
+  		@total_emissions = @electric_emissions + @heat_emissions
+  		@reduced_emissions = @reduced_electric_emissions + @reduced_heat_emissions
   		@emission_reduction = @total_emissions - @reduced_emissions
+  		@trees_planted = ((@emission_reduction * 12) / 2204.62) / 0.039
 
   		
 
