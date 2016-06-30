cd57f4043f9dd47c4632457ee0b761427a4b04e5
Pal Makkar <Pal@Pals-MacBook-Pro.local>
2016-06-30 14:04:45 -0700

    Added calculations for heating usage and reduced electricity and heating usahe

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,13 +8,17 @@ class EntriesController < ApplicationController
   	@zip = @entry.zip
   	@provider = @entry.provider
   	@usage = @entry.monthly_energy_usage
+  	@heating_usage = params[:entry][:fuel_usage].to_f
 
   	@providers = RateProvider.where(zip: @zip)
   	@subregion = ZipSubregion.find_by_zip(@zip)
   	#@rate = RateProvider.where(zip: @zip).first
   	if (@rate = RateProvider.where('zip = ? AND provider = ?', params[:entry][:zip], params[:entry][:provider]).first)
-  	
-  	 @bill = @usage * @rate.rate
+  		@bill = @usage * @rate.rate
+  		@reduced = @usage * (1 - (0.164 * 0.8))
+  		@heating_reduced = @heating_usage * 0.88
+
+
     else
   	 @error = "no providers available"
   
@@ -35,6 +39,6 @@ class EntriesController < ApplicationController
 	private
 
 	def entry_params
-		params.require(:entry).permit(:monthly_energy_usage, :zip, :provider)
+		params.require(:entry).permit(:monthly_energy_usage, :zip, :provider, :fuel_type, :fuel_usage)
 	end
 end
