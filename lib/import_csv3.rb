4b73946c471d381a0734dd52c8359a0e8475b158
Pal Makkar <Pal@Pals-MacBook-Pro.local>
2016-07-01 13:22:14 -0700

    Finished scripts for adding db tables to models

--- lib/import_csv3.rb
+++ lib/import_csv3.rb
@@ -1,14 +1,14 @@
 require 'csv'
 
-file_name = 'sub_emissions.csv'
+file_name = 'zip_sub.csv'
 
 path = "#{File.expand_path(File.dirname(__FILE__))}/#{file_name}"
 
 records = CSV.read(path, headers:true)
 records.each do |row|
-  SubregionEmission.create(
-    name: row['name'],
-    acronym: row['acronym'],
-    emissions: row['emissions']
+  ZipSubregion.create(
+    zip: row['zip'],
+    primary_sub: row['primary_sub'],
+    secondary_sub: row['secondary_sub']
   )
 end
