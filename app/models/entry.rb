d1773331176714bdd6cfa5b7fdef8b8a826da631
Pal Makkar <Pal@Pals-MacBook-Pro.local>
2016-07-06 15:06:00 -0700

    added units

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,7 @@
 class Entry < ActiveRecord::Base
+	validates :zip, presence: true
+	validates :monthly_energy_usage, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+	validates :provider, presence: true
+	validates :fuel_usage, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+	validates :fuel_type, presence: true
 end
