3a52744160fe5f530fb66161db2197d2bded7ca3
Pal Makkar <pmakkar97@gmail.com>
2016-07-14 19:27:42 -0700

    Added error messages

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,72 +5,69 @@ class EntriesController < ApplicationController
 
   def create
   	@entry = Entry.create(entry_params)
-  	@zip = @entry.zip
-  	@provider = @entry.provider
-  	@electric_usage = @entry.monthly_energy_usage
-  	@heating_usage = @entry.fuel_usage
-  	@fuel_type = @entry.fuel_type
+    if @entry.save
+    	@zip = @entry.zip
+    	@provider = @entry.provider
+    	@electric_usage = @entry.monthly_energy_usage
+    	@heating_usage = @entry.fuel_usage
+    	@fuel_type = @entry.fuel_type
 
-  	@subregion = ZipSubregion.find_by_zip(@zip)
+    	@subregion = ZipSubregion.find_by_zip(@zip)
 
-  	#Fuel information
-  	@fuel = FuelCost.find_by_fuel_type(@fuel_type)
-  	@heating_rate = @fuel.cost
-  	@raw_heating_emissions = @fuel.emissions
-  	@emission_conversion = @fuel.conversion
-  	@sr_emissions = SubregionEmission.find_by_acronym(@subregion.primary_sub)
+    	#Fuel information
+    	@fuel = FuelCost.find_by_fuel_type(@fuel_type)
+    	@heating_rate = @fuel.cost
+    	@raw_heating_emissions = @fuel.emissions
+    	@emission_conversion = @fuel.conversion
+    	@sr_emissions = SubregionEmission.find_by_acronym(@subregion.primary_sub)
 
-  	#If provider serves zip continue, else print error
-  	if (@rate = RateProvider.where('zip = ? AND provider = ?', params[:entry][:zip], params[:entry][:provider]).first)
+    	#If provider serves zip continue, else print error
+    	if (@rate = RateProvider.where('zip = ? AND provider = ?', params[:entry][:zip], params[:entry][:provider]).first)
 
-  		#Calculating total bill
-  		@electric_bill = @electric_usage * @rate.rate
-  		@heating_bill = @heating_usage * @heating_rate
-  		@total_bill = @electric_bill + @heating_bill
+    		#Calculating total bill
+    		@electric_bill = @electric_usage * @rate.rate
+    		@heating_bill = @heating_usage * @heating_rate
+    		@total_bill = @electric_bill + @heating_bill
 
-  		#Calculating reduced electric bill
-  		@electric_reduced = @electric_usage * (1 - (0.164 * 0.8))
-  		@reduced_electric_bill = @electric_reduced * (@rate.rate + 0.015)
+    		#Calculating reduced electric bill
+    		@electric_reduced = @electric_usage * (1 - (0.164 * 0.8))
+    		@reduced_electric_bill = @electric_reduced * (@rate.rate + 0.015)
 
-  		#Calculating reduced heating bill
-  		@heating_reduced = @heating_usage * 0.88
-  		@reduced_heating_bill = @heating_reduced * @heating_rate
+    		#Calculating reduced heating bill
+    		@heating_reduced = @heating_usage * 0.88
+    		@reduced_heating_bill = @heating_reduced * @heating_rate
 
-  		#Total savings
-  		@total_reduced_bill = @reduced_electric_bill + @reduced_heating_bill
-  		@total_savings = @total_bill - @total_reduced_bill
-      @yearly_savings = @total_savings * 12
-      @five_year_savings = @yearly_savings * 5
+    		#Total savings
+    		@total_reduced_bill = @reduced_electric_bill + @reduced_heating_bill
+    		@total_savings = @total_bill - @total_reduced_bill
+        @yearly_savings = @total_savings * 12
+        @five_year_savings = @yearly_savings * 5
 
 
 
-  		#Calculating reduced emissions
-  		@electric_emissions = (@electric_usage * @sr_emissions.emissions) / 1000
-  		@reduced_electric_emissions = ((@electric_reduced * @sr_emissions.emissions) / 1000)
-  		@heat_emissions = @raw_heating_emissions * @heating_usage * @emission_conversion
-  		@reduced_heat_emissions =  @raw_heating_emissions * @heating_reduced * @emission_conversion
+    		#Calculating reduced emissions
+    		@electric_emissions = (@electric_usage * @sr_emissions.emissions) / 1000
+    		@reduced_electric_emissions = ((@electric_reduced * @sr_emissions.emissions) / 1000)
+    		@heat_emissions = @raw_heating_emissions * @heating_usage * @emission_conversion
+    		@reduced_heat_emissions =  @raw_heating_emissions * @heating_reduced * @emission_conversion
 
-  		#Total reductions in emissions
-  		@total_emissions = @electric_emissions + @heat_emissions
-  		@reduced_emissions = @reduced_electric_emissions + @reduced_heat_emissions
-  		@emission_reduction = @total_emissions - @reduced_emissions
-      @monthly_emission_reduction = @emission_reduction / 12
-      @five_year_emission_reduction = @emission_reduction * 5
-  		@trees_planted = ((@emission_reduction * 12) / 2204.62) / 0.039
+    		#Total reductions in emissions
+    		@total_emissions = @electric_emissions + @heat_emissions
+    		@reduced_emissions = @reduced_electric_emissions + @reduced_heat_emissions
+    		@emission_reduction = @total_emissions - @reduced_emissions
+        @monthly_emission_reduction = @emission_reduction / 12
+        @five_year_emission_reduction = @emission_reduction * 5
+    		@trees_planted = ((@emission_reduction * 12) / 2204.62) / 0.039
 
-  		
+      else
+    	 @error = "no providers available"
+    	end
 
     else
-  	 @error = "no providers available"
-  	end
+      render "/entries/new"
+    end
 
-  	if @entry.persisted?
-			flash.now[:success] = "Your entry was recorded!"
-		else
-			flash.now[:danger] = "Entry not accepted."
-			render 'new'
-		end
-	end
+  end
 
   def rate_providers 
     @providers = RateProvider.where(zip: params[:zip].to_i)
