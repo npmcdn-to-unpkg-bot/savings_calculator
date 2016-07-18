9a5031fca72cc2f1d65ca9a4a11541927bd0229e
Pal Makkar <pmakkar97@gmail.com>
2016-07-18 14:32:10 -0700

    changed model for import

--- lib/import_csv4.rb
+++ lib/import_csv4.rb
@@ -1,12 +1,12 @@
 require 'csv'
 
-file_name = 'fuelcostraw.csv'
+file_name = 'fuelcost.csv'
 
 path = "#{File.expand_path(File.dirname(__FILE__))}/#{file_name}"
 
 records = CSV.read(path, headers:true)
 records.each do |row|
-  RateProvider.create(
+  FuelCost.create(
     fuel_type: row['fuel_type'],
     cost: row['cost'],
     conversion: row['conversion']
